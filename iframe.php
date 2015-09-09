d1a20dcdfbd82d91fbc2f920260705e50ec95a5e
Paco Rivera <paco.rivera@atom.com.gt>
2015-09-09 10:08:54 -0600

    iframe

---
+++ iframe.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+      <title>CycleSoup.com :: Your search engine for all engines (TM)</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width">
+      <meta name="robots" content="nofollow" />
+
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/slider.css">
+      <link rel="stylesheet" href="css/main.css">
+
+      <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+      <script src="js/vendor/jquery-1.10.1.min.js"></script>
+  </head>
+  <body>
+    <header id="site-header" class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header text-center float-none">
+          <a class="btn btn-default btn-callOA-contrast pull-left button-margin-top btn-sm" href="#" role="button"><span class="glyphicon glyphicon-chevron-left"></span> <span class="hidden-xs">Go</span> Back</a>
+          <a class="nav-logo inline-center" id="logo" href="/" title="Return to the homepage"><img src="img/cyclesoup-logo-white.png" alt="CycleSoup.com logo" /></a>
+          <a class="btn btn-default pull-right button-margin-top btn-sm" href="/index.php" role="button"><span class="glyphicon glyphicon-search"></span> New <span class="hidden-xs">Search</span></a>
+        </div>
+      </div>
+    </header>
+    
+    <iframe src="http://www.centurypwrsports.com/m/showcase_unit.htm?id=887533&status=2" style="width:100%; border: none;"></iframe>
+  
+    
+    <!-- javascript loaders -->
+    <!--script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script-->
+    <!--script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script-->
+    
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/vendor/bootstrap-slider.js"></script>
+    <script src="js/vendor/jquery.cookie.js"></script>
+    <script src="js/vendor/jquery.shuffle.min.js"></script>
+    <!--script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script-->
+    <script src="js/main.js"></script>
+    <!--script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src='//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script-->
+  </body>
+</html>
